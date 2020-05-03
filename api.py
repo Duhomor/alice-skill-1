23bebce7462b0148477c92bb23b202de77dc7318
Vitalij <50141715+Duhomor@users.noreply.github.com>
2020-05-03 22:13:47 +0300

    Update api.py

--- api.py
+++ api.py
@@ -51,8 +51,8 @@ def handle_dialog(req, res):
     
     if req['request']['original_utterance'].lower() in['эис-44']:
         #res['response']['text'] = '{}'.format(user_storage["event"])
-        #with open("eif.csv") as f_obj:
-        #csv_dict_reader(f_obj)
+        with open("eif.csv") as f_obj:
+        csv_dict_reader(f_obj)
         return
     elif req['request']['original_utterance'].lower() in['эис-45']:
         res['response']['text'] = 'Расписание для ЭИС-45'
@@ -62,5 +62,5 @@ def handle_dialog(req, res):
     
 def csv_dict_reader(file_obj):
     reader = csv.DictReader(file_obj, delimiter=';')
-    #for line in reader:
-        #res['response']['text'] = (line["time"])
+    for line in reader:
+        res['response']['text'] = (line["time"])
