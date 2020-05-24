1423d7c233c920cbeffb4645f0fd69ece95635c4
Vitalij <50141715+Duhomor@users.noreply.github.com>
2020-05-24 20:57:51 +0300

    Update api.py

--- api.py
+++ api.py
@@ -41,41 +41,67 @@ def main():
     )
 
 def handle_dialog(req, res):
-    user_storage = {}
+    storage = {}
     if req['session']['new']:
         res['response']['text'] = 'Привет! Я бот ЯГТУ. Я могу показать расписание занятий для твоей группы. В какой группе ты учишься?'
         return
         
     if req['request']['original_utterance'].lower() in['понедельник']:
-        user_storage["day"] = 'Понедельник'
-        res['response']['text'] = 'Расписание для ЭИС-45 {}'.format(user_storage["day"])
+        storage["day"] = 'Понедельник'
+        if storage["group"] != '':
+            res['response']['text'] = 'В какой группе ты учишься?'
+        else
+            res['response']['text'] = 'Расписание для {}'.format(storage["group"]) 'на {}'.format(storage["day"])
         return        
     if req['request']['original_utterance'].lower() in['вторник']:
-        user_storage["day"] = 'Вторник'
-        res['response']['text'] = 'Расписание для ЭИС-45'
+        storage["day"] = 'Вторник'
         return        
     if req['request']['original_utterance'].lower() in['среда']:
-        user_storage["day"] = 'Среда'
-        res['response']['text'] = 'Расписание для ЭИС-45'
+        storage["day"] = 'Среда'
         return        
     if req['request']['original_utterance'].lower() in['четверг']:
-        user_storage["day"] = 'Четверг'
-        res['response']['text'] = 'Расписание для ЭИС-45'
+        storage["day"] = 'Четверг'
         return        
     if req['request']['original_utterance'].lower() in['пятница']:
-        user_storage["day"] = 'Пятница'
-        res['response']['text'] = 'Расписание для ЭИС-45'
+        storage["day"] = 'Пятница'
         return        
     if req['request']['original_utterance'].lower() in['суббота']:
-        user_storage["day"] = 'Суббота'
-        res['response']['text'] = 'Расписание для ЭИС-45'
+        storage["day"] = 'Суббота'
         return
-    
+        
+    if req['request']['original_utterance'].lower() in['эмп-40']:
+        storage["group"] = 'эмп-40'
+        return    
+    if req['request']['original_utterance'].lower() in['эпи-41']:
+        storage["group"] = 'эпи-41'
+        return    
+    if req['request']['original_utterance'].lower() in['эсм-42']:
+        storage["group"] = 'эсм-42'
+        return    
+    if req['request']['original_utterance'].lower() in['эук-43']:
+        storage["group"] = 'эук-43'
+        return    
     if req['request']['original_utterance'].lower() in['эис-44']:
-        res['response']['text'] = 'Расписание для ЭИС-45'
+        storage["group"] = 'эис-44'
+        if storage["day"] != '':
+            res['response']['text'] = 'На какой день недели нужно найти расписание?'
+        else
+            res['response']['text'] = 'Расписание для {}'.format(storage["group"]) 'на {}'.format(storage["day"])
+        return    
+    if req['request']['original_utterance'].lower() in['эис-45']:
+        storage["group"] = 'эис-45'
+        return    
+    if req['request']['original_utterance'].lower() in['эмл-46']:
+        storage["group"] = 'эмл-46'
+        return    
+    if req['request']['original_utterance'].lower() in['эмгх-46']:
+        storage["group"] = 'эмгх-46'
         return
-    elif req['request']['original_utterance'].lower() in['эис-45']:
-        res['response']['text'] = 'Расписание для ЭИС-45'
+    if req['request']['original_utterance'].lower() in['ээ-47']:
+        storage["group"] = 'ээ-47'
+        return
+    elif req['request']['original_utterance'].lower() in['эи-48']:
+        storage["group"] = 'эи-48'
         return
     
     res['response']['text'] = 'Я не знаю такой группы. Попробуй ещё раз.'
