5c0d3261f8feff39d10ce165eb4fe83919d07063
Vitalij <50141715+Duhomor@users.noreply.github.com>
2020-05-24 21:12:50 +0300

    Update api.py

--- api.py
+++ api.py
@@ -48,53 +48,13 @@ def handle_dialog(req, res):
         
     if req['request']['original_utterance'].lower() in['понедельник']:
         storage["day"] = 'Понедельник'
+        res['response']['text'] = '{}'.format(storage["day"])
         return        
-    elif req['request']['original_utterance'].lower() in['вторник']:
-        storage["day"] = 'Вторник'
-        return        
-    elif req['request']['original_utterance'].lower() in['среда']:
-        storage["day"] = 'Среда'
-        return        
-    elif req['request']['original_utterance'].lower() in['четверг']:
-        storage["day"] = 'Четверг'
-        return        
-    elif req['request']['original_utterance'].lower() in['пятница']:
-        storage["day"] = 'Пятница'
-        return        
-    elif req['request']['original_utterance'].lower() in['суббота']:
-        storage["day"] = 'Суббота'
-        return
-        
-    elif req['request']['original_utterance'].lower() in['эмп-40']:
-        storage["group"] = 'эмп-40'
-        return    
-    elif req['request']['original_utterance'].lower() in['эпи-41']:
-        storage["group"] = 'эпи-41'
-        return    
-    elif req['request']['original_utterance'].lower() in['эсм-42']:
-        storage["group"] = 'эсм-42'
-        return    
-    elif req['request']['original_utterance'].lower() in['эук-43']:
-        storage["group"] = 'эук-43'
-        return    
-    elif req['request']['original_utterance'].lower() in['эис-44']:
-        storage["group"] = 'эис-44'
-        return    
-    elif req['request']['original_utterance'].lower() in['эис-45']:
-        storage["group"] = 'эис-45'
-        return    
-    elif req['request']['original_utterance'].lower() in['эмл-46']:
-        storage["group"] = 'эмл-46'
-        return    
-    elif req['request']['original_utterance'].lower() in['эмгх-46']:
-        storage["group"] = 'эмгх-46'
-        return
-    elif req['request']['original_utterance'].lower() in['ээ-47']:
-        storage["group"] = 'ээ-47'
-        return
-    elif req['request']['original_utterance'].lower() in['эи-48']:
-        storage["group"] = 'эи-48'
-        return
+
+    if req['request']['original_utterance'].lower() in['эис-44']:
+        storage["group"] = 'Эис-44'
+        res['response']['text'] = '{}'.format(storage["group"])
+        return     
     
     res['response']['text'] = 'Я не знаю такой группы. Попробуй ещё раз.'
     
