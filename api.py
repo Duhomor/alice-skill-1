c0e25fe4caf2ee88abd934114a0b213e6b45d77f
Vitalij <50141715+Duhomor@users.noreply.github.com>
2020-05-26 23:10:41 +0300

    Update api.py

--- api.py
+++ api.py
@@ -55,6 +55,11 @@ def handle_dialog(req, res):
         storage["group"] = 'эук-43'
         res['response']['text'] = 'Расписание на какой день недели тебя интересует?'   
         return
+                
+    if req['request']['original_utterance'].lower() in['ээ-47']:
+        storage["group"] = 'ээ-47'
+        res['response']['text'] = 'Расписание на какой день недели тебя интересует?'   
+        return
     
     if req['request']['original_utterance'].lower() in['понедельник']:
         storage["day"] = 'Понедельник'
@@ -71,8 +76,12 @@ def handle_dialog(req, res):
         res['response']['text'] = 'Этот день - выходной.'   
         return
     
-    if req['request']['original_utterance'].lower() in['спасибо']:
+    if req['request']['original_utterance'].lower() in['спасибо', 'благодарю']:
         res['response']['text'] = 'Рада помочь.'   
+        return
+    
+    if req['request']['original_utterance'].lower() in['помощь', 'помоги', 'help']:
+        res['response']['text'] = 'Для того, чтобы узнать расписание занятий для своей группы, просто скажи мне её название, например, "ЭИС-44", а затем назови день недели, на который нужно найти расписание.'   
         return
 
     res['response']['text'] = 'К сожалению, я не знаю такой команды. Попробуй ещё раз или скажи "помощь" для того, чтобы запросить правила навыка.'
