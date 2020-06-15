cfe7f1f4efa40978ce2410d4fa0b93f8760337fd
Vitalij <50141715+Duhomor@users.noreply.github.com>
2020-06-15 15:21:20 +0300

    Update api.py

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 # Импортируем модули для работы с JSON и логами.
 import json
 import logging
+import numpy
 
 # Импортируем подмодули Flask для запуска веб-сервиса.
 from flask import Flask, request
